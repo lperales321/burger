96d39aa2de1ac8f341e583bdf50795397721245a
Lorena Perales <lperales321@gmail.com>
2019-11-02 20:04:01 -0500

    Create db and table. Populate table

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Swiss Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheese Burger', false);
